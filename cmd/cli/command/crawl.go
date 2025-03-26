49e66fa1940d7ffa84db4648f10af92ee34e2c86
Emma <emmaLP@users.noreply.github.com>
2025-03-26 18:21:01 +0000

    Removing comma at the end of line within struct

--- cmd/cli/command/crawl.go
+++ cmd/cli/command/crawl.go
@@ -3,7 +3,7 @@ package command
 // Crawl struct contains the CLI args needed to start the crawl command
 type Crawl struct {
 	Domain string `short:"d" help:"Domain to web crawl. You don't need to include the scheme (https/http)"`
-	Scheme string `short:"s" optional:"" default:"https" help:"Overrides the scheme for the URL used. Defaults to https"`,
+	Scheme string `short:"s" optional:"" default:"https" help:"Overrides the scheme for the URL used. Defaults to https"`
 
 }
 
