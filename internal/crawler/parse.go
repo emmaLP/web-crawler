b70d021d59e48304d574c5123b53660194c767bb
Emma <emmaLP@users.noreply.github.com>
2025-03-31 16:58:34 +0100

    Updating the html parser to include handling relative paths and prefix with the base url

--- internal/crawler/parse.go
+++ internal/crawler/parse.go
@@ -4,14 +4,16 @@ import (
 	"io"
 	"net/url"
 	"slices"
+	"strings"
 
+	"github.com/rs/zerolog/log"
 	"golang.org/x/net/html"
 )
 
 // ExtractSameHostLinksFromHTML will take the html body within the reader and find any a tag that have an href attribute
 // This function will all ensure that the links returned from the page only match the specified host
 // Any links from external sources or subdomain on the request host will be ignored.
-func ExtractSameHostLinksFromHTML(body io.Reader, targetHost string) []string {
+func extractSameHostLinksFromHTML(body io.Reader, targetURL *url.URL) []string {
 	var links []string
 
 	htmTokenizer := html.NewTokenizer(body)
@@ -19,6 +21,7 @@ func ExtractSameHostLinksFromHTML(body io.Reader, targetHost string) []string {
 		currentPosition := htmTokenizer.Next()
 		//Check if reached the end of the input or an error has occurred
 		if currentPosition == html.ErrorToken {
+			log.Debug().Msgf("end of input")
 			// Sorting the URLs and then compacting them to remove any duplicate links
 			slices.Sort(links)
 			return slices.Compact(links)
@@ -28,9 +31,12 @@ func ExtractSameHostLinksFromHTML(body io.Reader, targetHost string) []string {
 		// Find any start tag or a self closing tag and only if if the token is `a`
 		if (currentPosition == html.StartTagToken || currentPosition == html.SelfClosingTagToken) && token.Data == "a" {
 			for _, attr := range token.Attr {
-				if attr.Key == "href" && isSameHostName(attr.Val, targetHost){
-						links = append(links, attr.Val)
+				if attr.Key == "href" {
+					link := parseReturnedLink(targetURL, attr.Val)
+					if isSameHostName(link, targetURL.Host) {
+						links = append(links, link)
 						break
+					}
 				}
 			}
 		}
@@ -38,8 +44,20 @@ func ExtractSameHostLinksFromHTML(body io.Reader, targetHost string) []string {
 
 }
 
-func isSameHostName(foundURL, targetHost string) bool{
-	found, err:= url.Parse(foundURL)
+func parseReturnedLink(base *url.URL, link string) string {
+	log.Print("Link:", link)
+	newLink := link
+	if strings.HasPrefix(link, "/") {
+		log.Print("Has prefix")
+		newLink = base.JoinPath(link).String()
+		log.Print("New link:", newLink)
+	}
+
+	return strings.TrimSuffix(newLink, "/")
+}
+
+func isSameHostName(foundURL, targetHost string) bool {
+	found, err := url.Parse(foundURL)
 	if err != nil {
 		return false
 	}
--- internal/crawler/parse_test.go
+++ internal/crawler/parse_test.go
@@ -1,65 +1,72 @@
-package crawler_test
+package crawler
 
 import (
 	"io"
+	"net/url"
 	"strings"
 	"testing"
 
-	"github.com/emmalp/web-crawler/internal/crawler"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_ExtractSameHostLinksFromHTML(t *testing.T) {
+	testURL, err := url.Parse("http://example.com")
+	assert.NoError(t, err)
 	tests := map[string]struct {
 		body          io.Reader
-		targetHost    string
+		targetURL    *url.URL
 		expectedLinks []string
 	}{
 		"empty reader": {
 			body:       strings.NewReader(""),
-			targetHost: "example.com",
+			targetURL: testURL,
 		},
 		"invalid html": {
 			body:       strings.NewReader("<html>hello/"),
-			targetHost: "example.com",
+			targetURL: testURL,
 		},
 		"a tag without href": {
 			body:       strings.NewReader("<html><a/></html>"),
-			targetHost: "example.com",
+			targetURL: testURL,
 		},
 		"single a tag with href": {
 			body:          strings.NewReader(`<html><a href="http://example.com"></a></html>`),
 			expectedLinks: []string{"http://example.com"},
-			targetHost:    "example.com",
+			targetURL:    testURL,
 		},
 		"multiple a tags - only match target host": {
 			body:          strings.NewReader(`<html><a href="http://example.com"></a><a href="http://google.com"></a></html>`),
 			expectedLinks: []string{"http://example.com"},
-			targetHost:    "example.com",
+			targetURL:    testURL,
 		},
 		"self closing href": {
 			body:          strings.NewReader(`<html><a href="http://example.com"/></html>`),
 			expectedLinks: []string{"http://example.com"},
-			targetHost:    "example.com",
+			targetURL:    testURL,
+		},
+		"relative link": {
+			body:          strings.NewReader(`<html><a href="/about-us"/></html>`),
+			expectedLinks: []string{"http://example.com/about-us"},
+			targetURL:    testURL,
 		},
 		"duplicate link- should only retrun once": {
 			body:          strings.NewReader(`<html><a href="http://example.com"/><body><a href="http://example.com"/></body></html>`),
 			expectedLinks: []string{"http://example.com"},
-			targetHost:    "example.com",
+			targetURL:    testURL,
 		},
 		"nested links": {
 			body:          strings.NewReader(`<html><a href="http://example.com"/><body><a href="http://example.com/test"/></body></html>`),
 			expectedLinks: []string{"http://example.com", "http://example.com/test"},
-			targetHost:    "example.com",
+			targetURL:    testURL,
 		},
 		"no href links": {
 			body:       strings.NewReader("<html><body><h1>Hello!</h1></body></html>"),
-			targetHost: "example.com",
+			targetURL: testURL,
 		},
 	}
 	for name, cfg := range tests {
 		t.Run(name, func(t *testing.T) {
-			links := crawler.ExtractSameHostLinksFromHTML(cfg.body, cfg.targetHost)
+			links := extractSameHostLinksFromHTML(cfg.body, cfg.targetURL)
 			assert.ElementsMatch(t, links, cfg.expectedLinks)
 		})
 	}
